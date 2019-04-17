839c0dc2d74b846c60bb711e692cde50db1292f5
dageqin <hdgeqin@126.com>
2019-04-17 20:13:30 +0800

    first commit

---
+++ pages/main/buy/index/index.js
@@ -0,0 +1,79 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      trees:[
+        {
+          num: 100000,
+          address:'临汾市',
+          date:'2019-04-16 15:16:01',
+          size:'长：5.7m，宽125mm,厚33mm',
+        },
+        {
+          num: 100000,
+          address: '临汾市',
+          date: '2019-04-16 15:16:01',
+          size: '长：5.7m，宽125mm,厚33mm',
+        }
+      ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
